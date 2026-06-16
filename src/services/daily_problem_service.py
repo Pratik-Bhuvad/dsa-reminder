6972ed1b7f543177fe70d5378dc948480de65084
Pratik Bhuvad <pbhuvad55@gmail.com>
2026-06-16 16:42:11 +0530

    feat: return same problem for repeated executions on the same day

--- src/services/daily_problem_service.py
+++ src/services/daily_problem_service.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
 from ..fetchers.json_fetcher import fetch_json_data
-from ..storage.history_repository import get_history, add_history, clear_history
+from ..storage.history_repository import (
+    get_history,
+    add_history,
+    clear_history,
+    get_history_by_date,
+)
 from ..selectors.problem_selector import select_todays_problem
 from ..outputs.terminal_output import log_problem_to_terminal
+
 
 def get_daily_problems(file_path):
     """
@@ -15,23 +21,42 @@ def get_daily_problems(file_path):
         list: A list of dictionaries representing daily problems.
     """
     available_problems = fetch_json_data(file_path)
-    attempted_problem = {entry['problem_id'] for entry in get_history()}
-    
+    attempted_problem = {entry["problem_id"] for entry in get_history()}
+
+    # Checking whether a problem has already been shown today
+    today = datetime.now().date()
+    existing_problem = get_history_by_date(today)
+
+    if existing_problem:
+        selected_problem = next(
+            (
+                problem
+                for problem in available_problems
+                if problem["id"] == existing_problem[0]["problem_id"]
+            ),
+            None,
+        )
+
+        log_problem_to_terminal(selected_problem)
+        return None
+
     # Filter out problems that have already been attempted
-    unattempted_problems = [problem for problem in available_problems if problem['id'] not in attempted_problem]
-    
+    unattempted_problems = [
+        problem
+        for problem in available_problems
+        if problem["id"] not in attempted_problem
+    ]
+
     # If all problems have been attempted, reset the history
     if not unattempted_problems:
         clear_history()
         unattempted_problems = available_problems
-        
-    # Select a problem to show to the user (for simplicity, we select the first unattempted problem)
-    
-    selected_problem  = select_todays_problem(unattempted_problems)
-    
+
+    selected_problem = select_todays_problem(unattempted_problems)
+
     # Record the selected problem in the history with the current date
-    add_history(selected_problem['id'], datetime.now())
-    
+    add_history(selected_problem["id"], datetime.now())
+
     log_problem_to_terminal(selected_problem)
-    
+
     return None
--- src/storage/history_repository.py
+++ src/storage/history_repository.py
@@ -9,6 +9,13 @@ def get_history():
         cursor.execute("SELECT * FROM problem_history")
         return cursor.fetchall()
     
+def get_history_by_date(date):
+    """Fetches the history of problems shown to the user on a specific date."""
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM problem_history WHERE DATE(shown_at) = ?", (date,))
+        return cursor.fetchall()
+    
 def add_history(problem_id, shown_at):
     """Adds a new entry to the problem history in the database."""
     with conn:
